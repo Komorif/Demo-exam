cfe64a3ed3ac5276df5527b5db148c9daa442fb6
Komori <121684959+Komorif@users.noreply.github.com>
2025-10-29 00:11:10 +0300

    init

---
+++ помощь/Решетников/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Решетников
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainFrame.Navigate(new AutorizationPage());
+        }
+    }
+}
---
+++ помощь/Решетников/Pages/AutorizationPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Linq;
+using Решетников.Database;
+
+namespace Решетников
+{
+    public partial class AutorizationPage : Page
+    {
+        public AutorizationPage()
+        {
+            InitializeComponent();
+            LoginButton.Click += LoginButton_Click;
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage("Гость"));
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordBox.Password;
+    
+            using (var context = new ReshetnikovEntities1())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Username == login && u.PasswordHash == password);
+
+                if (user != null)
+                {
+                    string roleName = user.Roles.RoleName;
+                    string fullName = user.FullName;
+
+                    NavigationService.Navigate(new MainPage(roleName, fullName));
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка авторизации",
+                                  MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
