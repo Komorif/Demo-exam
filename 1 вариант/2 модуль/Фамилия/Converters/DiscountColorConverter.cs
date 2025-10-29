ded9438739ca9379c87da788b9eda4318e89e6b2
Komori <121684959+Komorif@users.noreply.github.com>
2025-10-29 22:09:31 +0300

    пред законченный 1 вариант 2 модуль

--- 1 вариант/2 модуль/Фамилия/Converters/DiscountColorConverter.cs
+++ 1 вариант/2 модуль/Фамилия/Converters/DiscountColorConverter.cs
@@ -5,14 +5,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
+using Фамилия.DataBase;
 
 namespace Фамилия.Converters
 {
     internal class DiscountColorConverter : IValueConverter
     {
+        private static readonly SolidColorBrush GreenBrush = new SolidColorBrush(Color.FromRgb(0x2E, 0x8B, 0x57));
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            var item = value as Goods;
+            if (item == null)
+                return Brushes.White;
+
+            if (item.Orders.Count <= 0)
+                return Brushes.LightBlue;
+
+            if (item.Discount > 15)
+                return GreenBrush;
+
+            return Brushes.White;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- 1 вариант/2 модуль/Фамилия/Converters/PriceStyleConverter.cs
+++ 1 вариант/2 модуль/Фамилия/Converters/PriceStyleConverter.cs
@@ -4,7 +4,10 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
+using Фамилия.DataBase;
 
 namespace Фамилия.Converters
 {
@@ -12,7 +15,19 @@ namespace Фамилия.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            var item = value as Goods;
+            if (item == null)
+                return targetType == typeof(Brush) ? Brushes.Black : null;
+
+            bool hasDiscount = item.Discount > 0;
+
+            if (targetType == typeof(Brush))
+                return hasDiscount ? Brushes.Red : Brushes.Black;
+
+            if (targetType == typeof(TextDecorationCollection))
+                return hasDiscount ? TextDecorations.Strikethrough : null;
+
+            return null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
